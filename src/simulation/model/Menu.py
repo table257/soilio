7a1cd4700484a955894b8933d7ff81be4ecb6ba2
turtle144 <vttrung12@gmail.com>
2018-02-04 13:15:15 -0500

    This is a real commit

--- src/simulation/model/Menu.py
+++ src/simulation/model/Menu.py
@@ -1,34 +1,4 @@
-import model
-
-def startLoad(path):
-    farm = model.Farms.Farm.farmFromJson(path)
-    return farm
-
-# returns new farm of type 'arid', 'temperate', or 'boreal'
-def startNew(reg):
-    farm = model.Farms.Farm.makeFarm(reg, "../assets/regions.json")
-    return farm
-
-def iterate():
-    pass
-
-def plant(newPlant, plot):
-    plot.plant = newPlant
-
-def pick(plot):
-    if plot.plant == None:
-        return None
-    else:
-        plant = plot.plant;
-        plot.plant = None
-        return plant
-
-def addWater(plot, amtWater):
-    plot.water = plot.water + amtWater
-    return plot.water
-
-def fertilize(plot):
-    pass
+#!/usr/bin/python3
 
 def iterate(farm : Farm):
     farm.curIter += 1
